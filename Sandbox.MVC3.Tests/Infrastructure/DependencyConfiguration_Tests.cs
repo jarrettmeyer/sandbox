89183e5c3905128d3d3f5d25283e542930c2f2d5
Jarrett Meyer <jarrettmeyer@gmail.com>
2012-10-15 10:34:40 -0400

    Added unit tests for dependency configuration.

---
+++ Sandbox.MVC3.Tests/Infrastructure/DependencyConfiguration_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using Ninject;
+using Ninject.Parameters;
+using Sandbox.MVC3.Lib.Commands;
+using Sandbox.MVC3.Lib.Queries;
+
+namespace Sandbox.MVC3.Infrastructure
+{
+    [TestFixture]
+    public class DependencyConfiguration_Tests
+    {
+        private DependencyConfiguration configuration;
+        private IKernel kernel;
+
+        [SetUp]
+        public void before_each_test()
+        {
+            kernel = new StandardKernel();
+            configuration = new DependencyConfiguration(kernel);
+        }
+
+        [Test]
+        [TestCase(typeof(IClock), Result = true)]
+        [TestCase(typeof(ICommandStore), Result = true)]
+        [TestCase(typeof(IQueryStore), Result = true)]
+        public bool can_resolve_type(Type type)
+        {
+            ConfigureKernel();
+            var request = kernel.CreateRequest(type, x => true, new IParameter[] { }, false, false);
+            bool canResolveRequest = kernel.CanResolve(request);
+            return canResolveRequest;
+        }
+
+        private void ConfigureKernel()
+        {
+            configuration.Configure();
+        }
+    }
+}
