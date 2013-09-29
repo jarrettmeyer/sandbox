3eda6e0eb1c4c2f501c2dbd37cd108e1c6925e39
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-09-29 17:22:58 -0400

    adds stack class with tests

---
+++ Sandbox/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Sandbox
+{
+    public class Stack
+    {
+        private readonly int maxStackPointer;
+        private readonly int size;
+        private int stackPointer = -1;
+        private readonly object[] storage;
+
+        public Stack(int size)
+        {
+            if (size < 1)
+                throw new ArgumentOutOfRangeException("size", "Size cannot be less than 1.");
+
+            this.size = size;
+            maxStackPointer = size - 1;
+            storage = new object[size];
+        }
+
+        public int Size
+        {
+            get { return size; }
+        }
+
+        public void Push(object item)
+        {
+            if (stackPointer < maxStackPointer)
+            {
+                stackPointer += 1;
+                storage[stackPointer] = item;
+            }
+            else
+            {
+                throw new InvalidOperationException("The stack is full!");
+            }
+        }
+
+        public object Pop()
+        {
+            if (stackPointer >= 0)
+            {
+                object item = storage[stackPointer];
+                stackPointer -= 1;
+                return item;
+            }
+            else
+            {
+                throw new InvalidOperationException("The stack is empty!");
+            }
+        }
+    }
+}
---
+++ Sandbox/Stack_Tests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Sandbox
+{
+    [TestFixture]
+    public class Stack_Tests
+    {
+        [Test]
+        public void can_create_a_stack()
+        {
+            Stack stack = new Stack(2);
+            stack.Should().BeOfType<Stack>();
+        }
+
+        [Test]
+        public void can_get_stack_size()
+        {
+            Stack stack = new Stack(2);
+            stack.Size.Should().Be(2);
+        }
+
+        [Test]
+        public void can_push_onto_the_stack_as_many_times_as_size()
+        {
+            Stack stack = new Stack(3);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void cannot_create_an_empty_stack()
+        {
+            Action action = () => { new Stack(0); };
+            action.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Test]
+        public void popping_an_item_off_the_stack_returns_items_in_reverse_order()
+        {
+            Stack stack = new Stack(3);
+            stack.Push(1);
+            stack.Push(2);
+
+            object two = stack.Pop();
+            object one = stack.Pop();
+
+            one.Should().Be(1);
+            two.Should().Be(2);
+        }
+
+        [Test]
+        public void pushing_too_many_items_onto_the_stack_should_throw_an_exception()
+        {
+            Stack stack = new Stack(3);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Action action = () => { stack.Push(4); };
+
+            action.ShouldThrow<InvalidOperationException>();
+        }
+    }
+}
