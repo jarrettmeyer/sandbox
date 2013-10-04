00fb60e7a11f2da3fecf8f754e088b20172ec6d5
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-10-04 08:02:39 -0400

    adding caching library

---
+++ Sandbox.Caching/AspNetHttpCache.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Caching;
+
+namespace Sandbox.Caching
+{
+    public class AspNetHttpCache : ICache
+    {
+        public void Add(string key, object value)
+        {
+            HttpRuntime.Cache.Insert(key, value);
+        }
+
+        public void Add(string key, object value, DateTime expiration)
+        {
+            HttpRuntime.Cache.Insert(key, value, null, expiration, Cache.NoSlidingExpiration);
+        }
+
+        public void Add(string key, object value, TimeSpan duration)
+        {
+            HttpRuntime.Cache.Insert(key, value, null, Cache.NoAbsoluteExpiration, duration);
+        }
+
+        public object Get(string key)
+        {
+            return HttpRuntime.Cache.Get(key);
+        }
+
+        public T Get<T>(string key)
+        {
+            var value = Get(key);
+            if (value != null)
+                return (T)value;
+
+            return default(T);
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            HttpRuntime.Close();
+        }
+    }
+}
---
+++ Sandbox.Caching/ICache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sandbox.Caching
+{
+    /// <summary>
+    /// Represents a mechanism to store values.
+    /// </summary>
+    public interface ICache
+    {
+        /// <summary>
+        /// Add an item to the cache.
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <param name="value">Value</param>
+        void Add(string key, object value);
+
+        void Add(string key, object value, DateTime expiration);
+
+        void Add(string key, object value, TimeSpan duration);
+
+        object Get(string key);
+
+        T Get<T>(string key);
+
+        void Remove(string key);
+
+        void Clear();
+    }
+}
