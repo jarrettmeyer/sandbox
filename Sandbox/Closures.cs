0ac1073dcb4c32e6f7a89fd8183b4a3bf13f3c89
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-11-02 12:51:48 -0400

    updates to closures code

---
+++ Sandbox/Closures.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sandbox
+{
+    [TestFixture]
+    public class Closures
+    {
+        Action<int> myAction;
+
+        public int MyMethod(int value)
+        {
+            int i = 5;
+            myAction = (x) =>
+            {
+                int result = x + value;
+                i += result;
+            };
+            
+            myAction(3);
+
+            i += 2;
+            myAction(4);
+
+            i += 2;
+            myAction(5);
+
+            return i;
+        }
+
+        public Func<int, int> InitializeFunction(int value)
+        {
+            int i = 5;
+            return (x) =>
+            {
+                int result = x + value;
+                i += result;
+                return i;
+            };
+        }
+
+        public int MyOtherMethod()
+        {
+            var function = InitializeFunction(2);
+
+            int y = 1;
+            y += function(3);
+            y += function(4);
+            y += function(5);
+
+            return y;
+        }
+
+        [Test]
+        public void test_value_of_MyMethod()
+        {
+            Assert.AreEqual(27, MyMethod(2));
+        }
+
+        [Test]
+        public void test_value_of_MyOtherMethod()
+        {
+            Assert.AreEqual(50, MyOtherMethod());
+        }
+    }
+}
--- Sandbox/Delegates.cs
+++ Sandbox/Delegates.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Sandbox
 {
@@ -21,6 +22,40 @@ namespace Sandbox
             Sayer sayer = SayHello;
 
             Console.WriteLine(sayer("James"));
+        }
+
+        public delegate int StringFunction(string str);
+
+        public void MethodOne()
+        {
+            StringFunction myDelegate = delegate (string str)
+            {
+                if (string.IsNullOrWhiteSpace(str))
+                    return 0;
+
+                return str.Count(char.IsLetterOrDigit);
+            };
+
+            int result = myDelegate("Hello there!");
+            Console.WriteLine("The result is: " + result);
+        }
+
+        public void MethodTwo()
+        {
+            var result = "Hello World".Apply(RemoveWhitespace);
+        }
+
+        public string RemoveWhitespace(string str)
+        {
+            return "";
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string Apply(this string s, Func<string, string> function)
+        {
+            return function(s);
         }
     }
 }
